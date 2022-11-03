4a3c0c3c1b430c4dd2ec9cb79b9a1abdf40f4ef2
Leandro Medina <medinaleandron@gmail.com>
2022-11-03 06:37:10 -0300

    minor hotfix, removed test file

---
+++ dist/modules/auth/__test__/auth.module.test.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const module_1 = require("../module");
+const app = jest.fn();
+const controller = {
+    configureRoutes: jest.fn(),
+};
+const container = {
+    get: jest.fn(() => controller),
+};
+test('Auth module gets initialized correctly', () => {
+    (0, module_1.initAuthModule)(container, app);
+    expect(container.get).toHaveBeenCalledTimes(1);
+    expect(container.get).toHaveBeenCalledWith('AuthController');
+    expect(controller.configureRoutes).toHaveBeenCalledTimes(1);
+    expect(controller.configureRoutes).toHaveBeenCalledWith(app);
+});
